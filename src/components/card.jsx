d84a0c6dae77d5df34ac1c6c011fff4e2bc2783c
Deep Gajera <gajeradeep66@gmail.com>
2026-04-01 21:32:05 +0530

    files added

---
+++ src/components/card.jsx
@@ -0,0 +1,11 @@
+function card(){
+    
+    return (<>
+            <div className='card'>
+                <h1>Gajera Deep</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, voluptatibus.</p>
+            </div>
+    </>)
+}
+
+export default card
---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+    return (
+        <div>
+            <div className="data">
+                <h1>Hello i am navbar</h1>
+            </div>
+        </div>
+    )
+}
+
+export default navbar
