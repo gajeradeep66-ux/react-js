da532bda3dc7b6bea6994b4b5668b5499b9a067c
Deep Gajera <gajeradeep66@gmail.com>
2026-04-04 21:03:34 +0530

    added data

---
+++ UI_project_7/src/components/Section1/HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroText = () => {
+    return (
+        <div className='p-4'>
+                <h3 className='mb-8 text-7xl font-bold'> Procepective <br /><span className='text-gray-700 '>Customer</span> <br />Segmentation</h3>
+                <p className='text-xl font-medium text-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod repellat soluta saepe, similique minus nostrum?</p>
+            </div>
+    )
+}
+
+export default HeroText
